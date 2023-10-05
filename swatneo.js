30d2ad857852affc0be293451f2ffca443aa9ca1
jazza1251213 <132879220+jazza1251213@users.noreply.github.com>
2023-10-05 10:04:23 +0900

    Update swatneo.js

--- swatneo.js
+++ swatneo.js
@@ -1,1 +1,15 @@
-
+function checkAnswer() {
+    const answerInput = document.getElementById("answer");
+    const resultElement = document.getElementById("result");
+    
+    // 正しい答えをここで設定します
+    const correctAnswer = "正しい答え"; // 例: "〇" または "×"
+    
+    const userAnswer = answerInput.value.trim();
+    
+    if (userAnswer === correctAnswer) {
+        resultElement.textContent = "正解！";
+    } else {
+        resultElement.textContent = "不正解。もう一度試してみてください。";
+    }
+}
